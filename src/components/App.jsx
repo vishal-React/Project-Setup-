1e5dc39d69cd5b1becc791d8467558d784d1c7cd
Vishal Gupta <vishalgupta28012004@gmail.com>
2026-03-13 22:21:30 +0530

    remove react hot toast and add sonner toast

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,12 @@
+import { showSuccess } from "../toast/toast";
+
 const App = () => {
   return (
-    <div className="bg-emerald-400 text-black font-bold h-10 flex justify-center items-center">
+    <div className="bg-emerald-400 text-black font-bold h-10 flex justify-center items-center gap-2">
       Basic Setup Done with Folder Structure
+      <button onClick={() => showSuccess("hi")} className="cursor-pointer">
+        Click me
+      </button>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Toaster } from "react-hot-toast";
 import { RouterProvider } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { router } from "./routes/routes.jsx";
@@ -8,12 +7,13 @@ import { queryClient } from "./apiServices/configs/tanstackQueryConfig.js";
 import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary.jsx";
 import ErrorPage from "./components/ErrorPage/ErrorPage.jsx";
 import "./main.css";
+import { Toaster } from "sonner";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ErrorBoundary fallback={<ErrorPage />}>
       <QueryClientProvider client={queryClient}>
-        <Toaster />
+        <Toaster richColors closeButton theme="dark" duration={3000} />
         <RouterProvider router={router} />
       </QueryClientProvider>
     </ErrorBoundary>
